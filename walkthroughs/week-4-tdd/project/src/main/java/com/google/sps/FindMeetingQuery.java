2a829a1e82297f357bad04c95743f2a9d9338bfe
Giovanni Ferioli <gferioli0418@gmail.com>
2020-03-20 15:49:29 -0400

    All test were passed

--- walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -18,6 +18,8 @@ import java.util.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 
 public final class FindMeetingQuery {
@@ -26,57 +28,49 @@ public final class FindMeetingQuery {
     long duration = request.getDuration();
     Collection<TimeRange> ranges = new ArrayList<>();
 
+    //--------------------------------------------------------------------edge case
     // if duration too long
     if (duration > TimeRange.WHOLE_DAY.duration()) {
       return ranges;
     }
 
     // if no attendees
-    if (events.isEmpty() && attendees.isEmpty()) {
+    if (attendees.isEmpty()) {
       ranges.add(TimeRange.WHOLE_DAY);
       return ranges;
     }
 
-    // if only one attendee is in one event
-    if (events.size() == 1) {
-      Event firstElement = events.iterator().next();
-
-      TimeRange meetingDetails = firstElement.getWhen();
-      int start = meetingDetails.start();
-      int end = meetingDetails.end();
-
-      TimeRange before = TimeRange.fromStartEnd(TimeRange.START_OF_DAY, start, false);
-      TimeRange after = TimeRange.fromStartEnd(end, TimeRange.END_OF_DAY, true);
-
-      ranges.add(before);
-      ranges.add(after);
-      return ranges;
-    }
-
-    // if event>1
-    if (events.size() > 1) {
+    //----------------------------------------------------------------- general case
+    if (events.size() >= 0) {
       // iterate through each element in collection
       Iterator<Event> itr = events.iterator();
-      ArrayList<ArrayList<Integer>> meetings = new ArrayList<ArrayList<Integer>>(events.size());
-      int i = 0;
+      ArrayList<ArrayList<Integer>> meetings = new ArrayList<ArrayList<Integer>>();
 
       while (itr.hasNext()) {
-        // add the start time and the end time to the 2D array
+        // add the start time and the end time to the 2D arraylist
         Event element = itr.next();
-        TimeRange meetingDetails = element.getWhen();
-
-        ArrayList<Integer> temp = new ArrayList<Integer>(2);
-        temp.add(meetingDetails.start());
-        temp.add(meetingDetails.end());
-        meetings.add(temp);
-        i++;
+        Set<String> peopleInEvent = element.getAttendees();
+        // check if any person in the event matches a person in the request
+        List<String> common = new ArrayList<String>(attendees);
+        common.retainAll(peopleInEvent);
+        // if it does then add it to meetings
+        if (common.size() > 0) {
+          TimeRange meetingDetails = element.getWhen();
+          ArrayList<Integer> temp = new ArrayList<Integer>(2);
+          temp.add(meetingDetails.start());
+          temp.add(meetingDetails.end());
+          meetings.add(temp);
+        }
+      }
+      // if nothing in meetings list then put availability for meeting request for the whole day
+      if (meetings.size() == 0) {
+        ranges.add(TimeRange.WHOLE_DAY);
+        return ranges;
       }
 
-      // check if any times overlap
+      // replace any overlaps with one big meetings that includes what was overlaped
       removeOverlap(meetings);
-    for(int a = 0 ;a<meetings.size();a++){
-System.out.println(meetings.get(a).get(0)+","+meetings.get(a).get(1));
-    }
+
       TimeRange evt;
 
       // check if first event is in the beginning of day
@@ -86,31 +80,28 @@ System.out.println(meetings.get(a).get(0)+","+meetings.get(a).get(1));
       }
       // add the in between times
       for (i = 1; i < meetings.size(); i++) {
-        // check start time
+        // check if event end time is less than the next event start time
         if (meetings.get(i - 1).get(1) < meetings.get(i).get(0)) {
           evt = TimeRange.fromStartEnd(meetings.get(i - 1).get(1), meetings.get(i).get(0), false);
           ranges.add(evt);
         }
       }
 
+      // check if last event ends at the end of the day
       if (meetings.get(meetings.size() - 1).get(1) != 1440) {
-        //System.out.println(meetings.get(meetings.size() - 1).get(1));
-        evt = TimeRange.fromStartEnd(meetings.get(meetings.size() - 1).get(1), TimeRange.END_OF_DAY, true);
+        evt = TimeRange.fromStartEnd(
+            meetings.get(meetings.size() - 1).get(1), TimeRange.END_OF_DAY, true);
         ranges.add(evt);
       }
     }
-
     return ranges;
   }
 
   public void removeOverlap(ArrayList<ArrayList<Integer>> list) {
-    // sort events into ascending start time
-
-    // check if any events overlap meaning end time of previous event is greater than start time of
-    // current event
+    // removes any overlap
     for (int index = 1; index < list.size(); index++)
-      if ((list.get(index - 1).get(0) <= list.get(index).get(0))&& (list.get(index - 1).get(1) >= list.get(index).get(1)))
+      if ((list.get(index - 1).get(0) <= list.get(index).get(0))
+          && (list.get(index - 1).get(1) >= list.get(index).get(1)))
         list.remove(index);
-
   }
 }
